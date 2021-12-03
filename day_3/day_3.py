95d102c788e385ab264025ba06cd4846d4943e4d
Lykke Axlin <lykkeaxlin@hotmail.com>
2021-12-03 06:50:31 +0100

    ugly code but it at least it works now :)) #3

---
+++ day_3/day_3.py
@@ -0,0 +1,78 @@
+def part_one(measurements):
+    gamma, epsilon, counter = "", "", 0
+
+    for i in range(len(measurements[0])):
+        counter = 0
+
+        for measurement in measurements:
+            if measurement[i] == '1':
+                counter += 1
+
+        if counter >= len(measurements)/2:
+            gamma += '1'
+            epsilon += '0'
+        else:
+            gamma += '0'
+            epsilon += '1'
+
+    return int(gamma, 2) * int(epsilon, 2)
+
+
+def part_two(measurements):
+    orig_measurements = measurements
+    oxygen, co2 = "", ""
+
+    for i in range(len(measurements[0])):
+        counter = 0
+
+        for measurement in measurements:
+
+            if measurement[i] == '1':
+                counter += 1
+
+        if counter >= len(measurements)/2:
+            measurements = list(filter(lambda x: x[i] == '1', measurements))
+        else:
+            measurements = list(filter(lambda x: x[i] == '0', measurements))
+
+        if len(measurements) == 1:
+            oxygen = measurements[0]
+
+    measurements = orig_measurements
+
+    for i in range(len(measurements[0])):
+        counter = 0
+
+        for measurement in measurements:
+
+            if measurement[i] == '1':
+                counter += 1
+
+        if counter >= len(measurements)/2:
+            measurements = list(filter(lambda x: x[i] == '0', measurements))
+        else:
+            measurements = list(filter(lambda x: x[i] == '1', measurements))
+
+        if len(measurements) == 1:
+            co2 = measurements[0]
+
+    return int(oxygen, 2) * int(co2, 2)
+
+
+def read_input():
+    file = open("input.in", 'r')
+    measurements = []
+
+    for line in file.readlines():
+        measurements.append(line.strip())
+
+    return measurements
+
+
+def main():
+    measurements = read_input()
+    print("Part 1:", part_one(measurements))
+    print("Part 2:", part_two(measurements))
+
+
+main()
