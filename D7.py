2cc937c1f7c6927bf5778b2c71c758bca9c8e1e1
Lykke Axlin <lykkeaxlin@hotmail.com>
2021-12-07 06:59:30 +0100

    #/

---
+++ D7.py
@@ -0,0 +1,41 @@
+import statistics
+
+
+def part_one(positions):
+    median = int(statistics.median(positions))
+    fuel = 0
+
+    for nbr in positions:
+        fuel += abs(nbr - median)
+
+    return fuel
+
+
+# sum of natural numbers: [n(n+1)]/2
+def part_two(positions):
+    fuels = []
+
+    for x_pos in range(max(positions)):
+        fuel = 0
+
+        for pos in positions:
+            n = abs(pos - x_pos)
+            fuel += n * (n + 1) / 2
+
+        fuels.append(fuel)
+
+    return int(min(fuels))
+
+
+def read_input():
+    file = open("input.in", 'r')
+    return list(map(int, file.readline().split(",")))
+
+
+def main():
+    positions = read_input()
+    print("part 1:", part_one(positions))
+    print("part 2:", part_two(positions))
+
+
+main()
