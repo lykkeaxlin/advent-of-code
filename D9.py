8da2f2abc9c56ff5e227712fcbb3d807e85d210b
Lykke Axlin <lykkeaxlin@hotmail.com>
2021-12-09 20:50:57 +0100

    cleanup #9

--- D9.py
+++ D9.py
@@ -16,17 +16,13 @@ def part_one(points):
 
 # get neighbours and their corresponding coordinates for a given point
 def get_neigh_and_coords(i, j, points):
-    neighbours = []
-
     top     = (i-1, j) if i > 0 else (None, None)
     bottom  = (i+1, j) if i < len(points)-1 else (None, None)
     left    = (i, j-1) if j > 0 else (None, None)
     right   = (i, j+1) if j < len(points[0])-1 else (None, None)
 
     coordinates = list(filter(lambda x: x[0] is not None, [top, bottom, left, right]))
-
-    for i, j in coordinates:
-        neighbours.append(points[i][j])
+    neighbours = [points[coord[0]][coord[1]] for coord in coordinates]
 
     return neighbours, coordinates
 
