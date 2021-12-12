a1584908814cf6d236e2dff6aa79d1551d075ade
Lykke Axlin <lykkeaxlin@hotmail.com>
2021-12-12 14:11:00 +0100

    #11

---
+++ D11.py
@@ -0,0 +1,99 @@
+ROWS, COLS = 10, 10
+
+
+def flash(row, col, flashed, matrix, counter=0):
+    neighbours = get_neighbours(row, col)
+
+    for row, col in neighbours:
+        if (row, col) not in flashed:
+            matrix[row][col] += 1
+
+            if matrix[row][col] > 9:
+                flashed.add((row, col))
+                matrix[row][col] = 0
+                counter += 1
+                counter = flash(row, col, flashed, matrix, counter)
+
+    return counter
+
+
+def increment_board(points):
+    for row in range(ROWS):
+        for col in range(COLS):
+            points[row][col] += 1
+
+
+def has_all_flashed(points):
+    for row in range(ROWS):
+        for col in range(COLS):
+            if points[row][col] != 0:
+                return False
+    return True
+
+
+def get_neighbours(i, j):
+    coordinates = []
+    x_min, x_max = max(0, i - 1), min(ROWS - 1, i + 1)
+    y_min, y_max = max(0, j - 1), min(COLS - 1, j + 1)
+
+    for x in range(x_min, x_max + 1):
+        for y in range(y_min, y_max + 1):
+            if not (x == i and j == y):
+                coordinates.append((x, y))
+
+    return coordinates
+
+
+def part_one(matrix):
+    counter = 0
+
+    for _ in range(100):
+        increment_board(matrix)
+        flashed = set()
+
+        for row in range(ROWS):
+            for col in range(COLS):
+                if matrix[row][col] > 9:
+                    flashed.add((row, col))
+                    matrix[row][col] = 0
+                    counter += 1
+                    counter = flash(row, col, flashed, matrix, counter)
+
+    return counter
+
+
+def part_two(matrix):
+    step = 0
+
+    while not has_all_flashed(matrix):
+        increment_board(matrix)
+        flashed = set()
+
+        for row in range(ROWS):
+            for col in range(COLS):
+                if matrix[row][col] > 9:
+                    flashed.add((row, col))
+                    matrix[row][col] = 0
+                    flash(row, col, flashed, matrix)
+        step += 1
+
+    return step
+
+
+def read_input():
+    file = open("input.in", 'r')
+    input = []
+
+    for line in file.readlines():
+        numbers = [int(a) for a in line.strip()]
+        input.append(numbers)
+
+    return input
+
+
+def main():
+    print("Part 1:", part_one(read_input()))
+    print("Part 2:", part_two(read_input()))
+
+
+main()
