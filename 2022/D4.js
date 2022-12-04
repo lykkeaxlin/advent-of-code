a40a64bdf96a6441bf94dddbc9a807620b0cd6c1
Lykke Axlin <lykkeaxlin@hotmail.com>
2022-12-04 11:25:47 +0100

    D4

---
+++ 2022/D4.js
@@ -0,0 +1,73 @@
+var fs = require("fs");
+
+const test = false;
+
+const readFile = () => {
+  return fs
+    .readFileSync(test ? "test.in" : "input.in")
+    .toString()
+    .split("\n");
+};
+
+const getRange = (start, end) => {
+  return [...Array(end - start + 1)].map((_, i) => start + i);
+};
+
+const isFullyContained = (first, second) => {
+  const firstContainsSecond =
+    new Set([...first].filter((element) => second.has(element))).size ===
+    second.size;
+
+  const secondContainsFirst =
+    new Set([...second].filter((element) => first.has(element))).size ===
+    first.size;
+
+  return firstContainsSecond || secondContainsFirst;
+};
+
+const firstPart = (input) => {
+  let count = 0;
+  input.forEach((element) => {
+    const split = element.split(",");
+    const f1 = parseInt(split[0].split("-")[0]);
+    const f2 = parseInt(split[0].split("-")[1]);
+    const s1 = parseInt(split[1].split("-")[0]);
+    const s2 = parseInt(split[1].split("-")[1]);
+
+    const firstRange = getRange(f1, f2);
+    const secondRange = getRange(s1, s2);
+
+    if (isFullyContained(new Set(firstRange), new Set(secondRange))) {
+      count++;
+    }
+  });
+  return count;
+};
+
+const isOverlapping = (first, second) => {
+  return new Set([...first].filter((element) => second.has(element))).size > 0;
+};
+
+const secondPart = (input) => {
+  let count = 0;
+  input.forEach((element) => {
+    const split = element.split(",");
+    const f1 = parseInt(split[0].split("-")[0]);
+    const f2 = parseInt(split[0].split("-")[1]);
+    const s1 = parseInt(split[1].split("-")[0]);
+    const s2 = parseInt(split[1].split("-")[1]);
+
+    const firstRange = getRange(f1, f2);
+    const secondRange = getRange(s1, s2);
+
+    if (isOverlapping(new Set(firstRange), new Set(secondRange))) {
+      count++;
+    }
+  });
+  return count;
+};
+
+const input = readFile();
+
+console.log("first", firstPart(input));
+console.log("second", secondPart(input));
