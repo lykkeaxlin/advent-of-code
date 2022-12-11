59906044b2eea83fc6a120239bb792709d0dc791
Lykke Axlin <lykkeaxlin@hotmail.com>
2022-12-11 11:44:43 +0100

    D11 P1

---
+++ 2022/D11.js
@@ -0,0 +1,73 @@
+var fs = require("fs");
+
+const test = false;
+
+const readFile = () => {
+  return fs
+    .readFileSync(test ? "test.in" : "input.in")
+    .toString()
+    .split("\n");
+};
+
+const createMonkeys = (input) => {
+  const monkyes = [];
+  const chunkSize = 7;
+
+  for (let i = 0; i < input.length; i += chunkSize) {
+    let monkey = {};
+    const chunk = input.slice(i, i + chunkSize);
+    monkey.startingItems = chunk[1].split(":")[1].split(",").map(Number);
+    monkey.op = chunk[2].split(" ")[6];
+    monkey.opVal = chunk[2].split(" ")[7];
+    monkey.test = parseInt(chunk[3].split(" ")[5]);
+    monkey.trueThrow = parseInt(chunk[4].split(" ").slice(-1)[0]);
+    monkey.falseThrow = parseInt(chunk[5].split(" ").slice(-1)[0]);
+    monkey.count = 0;
+    monkyes.push(monkey);
+  }
+  return monkyes;
+};
+
+const calcWorryLevel = (old, op, opVal) => {
+  if (op === "*") {
+    return isNaN(parseInt(opVal)) ? old * old : old * parseInt(opVal);
+  }
+  return isNaN(parseInt(opVal)) ? old + old : old + parseInt(opVal);
+};
+
+const getLevel = (monkeys) => {
+  const levels = monkeys
+    .map((monkey) => monkey.count)
+    .sort()
+    .reverse();
+  return levels[0] * levels[1];
+};
+
+const firstPart = (input) => {
+  const monkeys = createMonkeys(input);
+
+  for (var i = 0; i < 20; i++) {
+    monkeys.forEach((monkey) => {
+      monkey.startingItems.forEach((item) => {
+        let worryLevel = calcWorryLevel(item, monkey.op, monkey.opVal);
+        worryLevel = Math.floor(worryLevel / 3);
+        monkey.count += 1;
+
+        if (worryLevel % monkey.test === 0) {
+          monkeys[monkey.trueThrow].startingItems.push(worryLevel);
+        } else {
+          monkeys[monkey.falseThrow].startingItems.push(worryLevel);
+        }
+      });
+      monkey.startingItems = [];
+    });
+  }
+  return getLevel(monkeys);
+};
+
+const secondPart = (input) => {};
+
+const input = readFile();
+
+console.log("first:", firstPart(input));
+console.log("second:", secondPart(input));
