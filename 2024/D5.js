6e5076232ab11aede59856a7fdfa9dd9f5708a38
Lykke Axlin <46992305+lykkeaxlin@users.noreply.github.com>
2024-12-16 20:36:49 +0100

    cleanup

--- 2024/D5.js
+++ 2024/D5.js
@@ -1,6 +1,6 @@
 import { readFileSync } from 'fs';
 
-const test = true;
+const test = false;
 
 const readFile = () =>
   readFileSync(test ? 'test.in' : 'input.in')
@@ -20,70 +20,35 @@ const parseInput = (input) => {
         rules[n1] = [n2];
       }
     } else if (element !== '') {
-      updates.push(element);
+      updates.push(element.split(',').map(Number));
     }
   });
 
-  return [rules, updates];
+  return { rules, updates };
 };
 
-const isCorrectlyOrdered = (rules, nbrs) => {
-  for (let i = 0; i < nbrs.length; i++) {
-    const nbr = nbrs[i];
-    const list = nbrs.slice(0, i);
-    if (rules[nbr]?.some((x) => list.includes(x))) {
-      return false;
-    }
-  }
-  return true;
-};
+const isCorrectlyOrdered = (rules, nbrs) =>
+  nbrs.every(
+    (nbr, i) => !rules[nbr]?.some((x) => nbrs.slice(0, i).includes(x))
+  );
 
-const firstPart = (input) => {
-  const [rules, updates] = parseInput(input);
-  var sum = 0;
-
-  updates.forEach((update) => {
-    const nbrs = update.split(',').map(Number);
-    var correct = true;
-
-    for (let i = 0; i < nbrs.length; i++) {
-      if (!isCorrectlyOrdered(rules, nbrs)) {
-        correct = false;
-        break;
-      }
-    }
-
-    if (correct) {
-      sum += nbrs[Math.floor(nbrs.length / 2)];
-    }
-  });
-
-  return sum;
-};
+const firstPart = ({ rules, updates }) =>
+  updates
+    .filter((x) => isCorrectlyOrdered(rules, x))
+    .reduce((sum, x) => sum + x[Math.floor(x.length / 2)], 0);
 
 const reorder = (rules, nbrs) =>
   nbrs.sort((a, b) =>
     rules[a]?.includes(b) ? -1 : rules[b]?.includes(a) ? 1 : 0
   );
 
-const secondPart = (input) => {
-  const [rules, updates] = parseInput(input);
-  let sum = 0;
-
-  updates.forEach((update) => {
-    const nbrs = update.split(',').map(Number);
-
-    if (!isCorrectlyOrdered(rules, nbrs)) {
-      const reordered = reorder(rules, nbrs);
-
-      sum += reordered[Math.floor(reordered.length / 2)];
-    }
-  });
-
-  return sum;
-};
+const secondPart = ({ rules, updates }) =>
+  updates
+    .filter((x) => !isCorrectlyOrdered(rules, x))
+    .map((x) => reorder(rules, x))
+    .reduce((sum, x) => sum + x[Math.floor(x.length / 2)], 0);
 
 const input = readFile();
 
-console.log('first:', firstPart(input));
-console.log('second:', secondPart(input));
+console.log('first:', firstPart(parseInput(input)));
+console.log('second:', secondPart(parseInput(input)));
